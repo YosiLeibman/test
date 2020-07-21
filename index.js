8bbb779401dcc0fa8700958051173dd436a00d50
Your Name Here <your@email.com>
2020-07-21 16:26:30 +0300

    just messing around

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log("hey i was borm in the secondary branch");
 console.log("hey i was borm in the secondary branch");
 console.log("hey i was borm in the secondary branch");
 console.log("hey i was borm in the secondary branch");
-console.log("hey i was borm in the secondary branch");
+console.log("the secondary branch");
